ad12d4c8b71bd0242e5266c51b3098053dfce9f2
rufane1996 <rezizova544@gmail.com>
2026-03-28 16:34:01 +0400

    Deploy website - based on 40e54d36385a6d9731b2042bd076315c3a7dd10f

--- assets/js/acecf23e.e12d5494.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkwpsocialjet=globalThis.webpackChunkwpsocialjet||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/wpsocialjet/blog","blogTitle":"Blog","authorsListPath":"/wpsocialjet/blog/authors"}')}}]);
